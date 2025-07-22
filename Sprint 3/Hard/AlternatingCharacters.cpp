3a55e5831584f17d782e9e19dc1e54b8abedbe69
Kratik777 <kratikbhatt6@gmail.com>
2025-07-22 05:38:28 +0530

    updated

--- Sprint 3/Hard/AlternatingCharacters.cpp
+++ Sprint 3/Hard/AlternatingCharacters.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 using namespace std;
 
-//Function to print alternating characters pattern.
-void alternatingCharacters(int n){
-    char ch = 65;
-    char ch1 = 66;
-    for(int row = 0 ; row < n; row++){
-        for(int col = 0 ; col < n ; col++){
-            if((row % 2 == 0 && col % 2 == 0) || (row%2!=0 && col%2!= 0)){
-                cout<<ch<<" ";
-            }
-            else{
-                cout<<ch1<<" ";
-            }
-        }
-        cout<<endl;
+// Function to print alternating characters pattern.
+void alternatingCharacters(int n) {
+  char ch = 65;
+  char ch1 = 67;
+  for (int row = 0; row < n; row++) {
+    for (int col = 0; col < n; col++) {
+      if ((row % 2 == 0 && col % 2 == 0) || (row % 2 != 0 && col % 2 != 0)) {
+        cout << ch << " ";
+      } else {
+        cout << ch1 << " ";
+      }
     }
+    cout << endl;
+  }
 }
 
 int main() {
-    int rows ;
-    cin>>rows;
-    alternatingCharacters(rows);
-    return 0;
+  int rows;
+  cin >> rows;
+  alternatingCharacters(rows);
+  return 0;
 }
